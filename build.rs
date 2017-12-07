0238e91df9353b9d3b248918c82858f321ef78fd
Joshua Karns <jkarns275@gmail.com>
2017-12-07 14:23:46 -0500

    In order to get the program to compile on windows, gfortran must be installed (confirmed to work with MSYS2 on 64 bit windows 10).

--- build.rs
+++ build.rs
@@ -4,6 +4,7 @@ fn main() {
     //gcc::Config::new().file("src/f80.c").flag("-lmath").compile("libf80.a");("libf80.a", &["src/f80.c"]);
     cc::Build::new()
         .file("src/f128.c")
+        .flag("-Bstatic")
         .flag("-lquadmath")
         .compile("libf128.a");
     //("libf128.a", &["src/f128.c"]);
