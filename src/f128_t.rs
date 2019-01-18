a2635e7026aef88404f28d27fe1d3f3a059ca2c6
Joshua Karns <jkarns275@gmail.com>
2019-01-18 13:58:09 -0500

    Fixed conversion fns

--- src/f128_t.rs
+++ src/f128_t.rs
@@ -39,77 +39,154 @@ impl f128 {
 
 
     #[cfg(target_endian = "big")]
-    pub const SIGN_BIT: f128 = f128([0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+    pub const SIGN_BIT: f128 =
+        f128([0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+              0x00, 0x00, 0x00, 0x00, 0x00]);
     #[cfg(target_endian = "little")]
-    pub const SIGN_BIT: f128 = f128([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80]);
+    pub const SIGN_BIT: f128 =
+        f128([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+              0x00, 0x00, 0x00, 0x00, 0x80]);
 
     #[cfg(target_endian = "big")]
-    pub const EXPONENT_BITS: f128 = f128([0x7f, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+    pub const EXPONENT_BITS: f128 =
+        f128([0x7f, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+              0x00, 0x00, 0x00, 0x00, 0x00]);
     #[cfg(target_endian = "little")]
-    pub const EXPONENT_BITS: f128 = f128([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0x7f]);
+    pub const EXPONENT_BITS: f128 =
+        f128([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+              0x00, 0x00, 0x00, 0xff, 0x7f]);
 
     #[cfg(target_endian = "big")]
-    pub const FRACTION_BITS: f128 = f128([0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]);
+    pub const FRACTION_BITS: f128 =
+        f128([0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+              0xFF, 0xFF, 0xFF, 0xFF, 0xFF]);
     #[cfg(target_endian = "little")]
-    pub const FRACTION_BITS: f128 = f128([0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00]);
+    pub const FRACTION_BITS: f128 =
+        f128([0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+              0xFF, 0xFF, 0xFF, 0x00, 0x00]);
 
     #[cfg(target_endian = "big")]
-    pub const MIN: f128 = f128([0xff, 0xfe, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]);
+    pub const MIN: f128 =
+        f128([0xff, 0xfe, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+              0xff, 0xff, 0xff, 0xff, 0xff]);
     #[cfg(target_endian = "little")]
-    pub const MIN: f128 = f128([0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfe, 0xff]);
+    pub const MIN: f128 =
+        f128([0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+              0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfe, 0xff]);
 
     #[cfg(target_endian = "big")]
-    pub const MIN_POSITIVE: f128 = f128([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01]);
+    pub const MIN_POSITIVE: f128 =
+        f128([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+              0x00, 0x00, 0x00, 0x00, 0x01]);
     #[cfg(target_endian = "little")]
-    pub const MIN_POSITIVE: f128 = f128([0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+    pub const MIN_POSITIVE: f128 =
+        f128([0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+              0x00, 0x00, 0x00, 0x00, 0x00]);
 
     #[cfg(target_endian = "big")]
-    pub const ONE: f128 = f128([0x3f, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+    pub const ONE: f128 =
+        f128([0x3f, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+              0x00, 0x00, 0x00, 0x00, 0x00]);
     #[cfg(target_endian = "little")]
-    pub const ONE: f128 = f128([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0x3f]);
+    pub const ONE: f128 =
+        f128([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+              0x00, 0x00, 0x00, 0xff, 0x3f]);
 
     #[cfg(target_endian = "big")]
-    pub const TWO: f128 = f128([0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+    pub const TWO: f128 =
+        f128([0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+              0x00, 0x00, 0x00, 0x00, 0x00]);
     #[cfg(target_endian = "little")]
-    pub const TWO: f128 = f128([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x40]);
+    pub const TWO: f128 =
+        f128([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+              0x00, 0x00, 0x00, 0x00, 0x40]);
 
     #[cfg(target_endian = "big")]
-    pub const E: f128 = f128([0x40, 0x00, 0x5b, 0xf0, 0xa8, 0xb1, 0x45, 0x76, 0x95, 0x35, 0x5f, 0xb8, 0xac, 0x40, 0x4e, 0x7a]);
+    pub const E: f128 =
+        f128([0x40, 0x00, 0x5b, 0xf0, 0xa8, 0xb1, 0x45, 0x76, 0x95, 0x35, 0x5f,
+              0xb8, 0xac, 0x40, 0x4e, 0x7a]);
     #[cfg(target_endian = "little")]
-    pub const E: f128 = f128([0x7a, 0x4e, 0x40, 0xac, 0xb8, 0x5f, 0x35, 0x95, 0x76, 0x45, 0xb1, 0xa8, 0xf0, 0x5b, 0x00, 0x40]);
+    pub const E: f128 =
+        f128([0x7a, 0x4e, 0x40, 0xac, 0xb8, 0x5f, 0x35, 0x95, 0x76, 0x45, 0xb1,
+              0xa8, 0xf0, 0x5b, 0x00, 0x40]);
 
     #[cfg(target_endian = "big")]
-    pub const PI: f128 = f128([0x40, 0x00, 0x92, 0x1f, 0xb5, 0x44, 0x42, 0xd1, 0x84, 0x69, 0x89, 0x8c, 0xc5, 0x17, 0x01, 0xb8]);
+    pub const PI: f128 =
+        f128([0x40, 0x00, 0x92, 0x1f, 0xb5, 0x44, 0x42, 0xd1, 0x84, 0x69, 0x89,
+              0x8c, 0xc5, 0x17, 0x01, 0xb8]);
     #[cfg(target_endian = "little")]
-    pub const PI: f128 = f128([0xb8, 0x01, 0x17, 0xc5, 0x8c, 0x89, 0x69, 0x84, 0xd1, 0x42, 0x44, 0xb5, 0x1f, 0x92, 0x00, 0x40]);
+    pub const PI: f128 =
+        f128([0xb8, 0x01, 0x17, 0xc5, 0x8c, 0x89, 0x69, 0x84, 0xd1, 0x42, 0x44,
+              0xb5, 0x1f, 0x92, 0x00, 0x40]);
 
     #[cfg(target_endian = "little")]
-    pub const INFINITY: f128 = f128([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0x7f]);
+    pub const INFINITY: f128 =
+        f128([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0x7f]);
     #[cfg(target_endian = "big")]
-    pub const INFINITY: f128 = f128([0x7F, 0xFF, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    pub const INFINITY: f128 =
+        f128([0x7F, 0xFF, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
     #[cfg(target_endian = "big")]
-    pub const NAN: f128 = f128([0x7F, 0xFF, 0xFF,  0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFFu8, 0xFFu8, 0xFF ]);
+    pub const NAN: f128 =
+        f128([0x7F, 0xFF, 0xFF,  0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+              0xFF, 0xFF, 0xFF, 0xFFu8, 0xFFu8, 0xFF]);
     #[cfg(target_endian = "little")]
-    pub const NAN: f128 = f128([ 0xFF, 0xFF, 0xFF,  0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFFu8, 0xFFu8, 0x7F ]);
+    pub const NAN: f128 =
+        f128([0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+              0xFF, 0xFF, 0xFFu8, 0xFFu8, 0x7F]);
 
     #[cfg(target_endian = "little")]
-    pub const NEG_INFINITY: f128 = f128([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0xff]);
+    pub const NEG_INFINITY: f128 =
+        f128([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0xff]);
     #[cfg(target_endian = "big")]
-    pub const NEG_INFINITY: f128 = f128([0xff, 0xff, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    pub const NEG_INFINITY: f128 =
+        f128([0xff, 0xff, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+    #[cfg(target_endian = "little")]
+    pub const EPSILON: f128 =
+        f128([0x65, 0x64, 0xD2, 0x5B, 0x93, 0xF2, 0x61, 0x43, 0, 0x51, 0x2F,
+              0x7F, 0x8A, 0, 0x8F, 0x3F]);
+    #[cfg(target_endian = "big")]
+    pub const EPSILON: f128 =
+        f128([0x3F, 0x8F, 0x0, 0x8A, 0x7F, 0x2F, 0x51, 0x0, 0x43, 0x61, 0xF2,
+              0x93, 0x5B, 0xD2, 0x64, 0x65]);
+
+    #[cfg(target_endian = "little")]
+    pub const NEG_ZERO: f128 =
+        f128([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x80]);
+    #[cfg(target_endian = "big")]
+    pub const NEG_ZERO: f128 =
+        f128([0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+    #[cfg(target_endian = "little")]
+    pub const MAX: f128 =
+        f128([0x7f, 0xfe, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+              0xff, 0xff, 0xff, 0xff, 0xff]);
+    #[cfg(target_endian = "big")]
+    pub const MAX: f128 =
+        f128([0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+              0xff, 0xff, 0xff, 0xfe, 0x7f]);
 
     pub(crate) fn from_arr(d: [u8; 16]) -> Self { f128(d) }
 
     #[inline(always)]
-    pub fn from_raw_u128(d: u128) -> Self { f128::from_arr(unsafe { mem::transmute::<u128, [u8; 16]>(d) }) }
+    pub(crate) fn from_raw_u128(d: u128) -> Self {
+        f128::from_arr(unsafe { mem::transmute::<u128, [u8; 16]>(d) })
+    }
     #[inline(always)]
-    pub fn from_raw_i128(d: i128) -> Self { f128::from_arr(unsafe { mem::transmute::<i128, [u8; 16]>(d) }) }
+    pub(crate) fn from_raw_i128(d: i128) -> Self {
+        f128::from_arr(unsafe { mem::transmute::<i128, [u8; 16]>(d) })
+    }
 
     #[inline(always)]
-    pub fn inner_as_i128(self) -> i128 { unsafe { mem::transmute::<[u8; 16], i128>(self.0) } }
+    pub(crate) fn inner_as_i128(self) -> i128 {
+        unsafe { mem::transmute::<[u8; 16], i128>(self.0) }
+    }
 
     #[inline(always)]
-    pub fn inner_as_u128(&self) -> u128 { unsafe { mem::transmute::<[u8; 16], u128>(self.0) } }
+    pub(crate) fn inner_as_u128(&self) -> u128 {
+        unsafe { mem::transmute::<[u8; 16], u128>(self.0) }
+    }
 
     #[inline(always)]
     pub fn new<T: Into<f128>>(a: T) -> Self { a.into() }
@@ -125,7 +202,9 @@ impl f128 {
             Ok(e) => cstr = e,
             Err(_) => return None
         };
-        let n = unsafe { qtostr((&mut buf).as_mut_ptr(), 128, cstr.as_ptr(), self.inner()) };
+        let n = unsafe {
+            qtostr((&mut buf).as_mut_ptr(), 128, cstr.as_ptr(), self.clone().inner())
+        };
         let mut v = Vec::with_capacity(n as usize);
         for i in 0..n {
             v.push(buf[i as usize]);
@@ -134,7 +213,7 @@ impl f128 {
     }
 
     #[inline(always)]
-    pub fn inner(&self) -> [u8; 16] { self.0 }
+    pub fn inner(&self) -> [u8; 16] { self.0.clone() }
 
     #[inline(always)]
     pub fn into_inner(self) -> [u8; 16] { self.0 }
@@ -147,7 +226,8 @@ impl f128 {
     }
 
     pub fn exp_bits(&self) -> u32 {
-        ((self.inner_as_u128() & f128::EXPONENT_BITS.inner_as_u128()) >> 112) as u32
+        let exp_bits = f128::EXPONENT_BITS.inner_as_u128();
+        ((self.inner_as_u128() & exp_bits) >> 112) as u32
     }
 
     pub fn fract_bits(&self) -> u128 {
@@ -165,70 +245,124 @@ impl One for f128 {
 }
 
 impl ToPrimitive for f128 {
-    fn to_i64(&self)    -> Option<i64> { Some(unsafe { f128_to_i64(self.inner()) }) }
-    fn to_u64(&self)    -> Option<u64> { Some(unsafe { f128_to_u64(self.inner()) }) }
-    fn to_isize(&self)  -> Option<isize> { Some(unsafe { f128_to_i64(self.inner()) as isize }) }
-    fn to_i8(&self)     -> Option<i8> { Some(unsafe { f128_to_i8(self.inner()) }) }
-    fn to_i16(&self)    -> Option<i16> { Some(unsafe { f128_to_i16(self.inner()) }) }
-    fn to_i32(&self)    -> Option<i32> { Some(unsafe { f128_to_i32(self.inner()) }) }
-    fn to_usize(&self)  -> Option<usize> { Some(unsafe { f128_to_u64(self.inner()) as usize }) }
-    fn to_u8(&self)     -> Option<u8> { Some(unsafe { f128_to_u8(self.inner()) }) }
-    fn to_u16(&self)    -> Option<u16> { Some(unsafe { f128_to_u16(self.inner()) }) }
-    fn to_u32(&self)    -> Option<u32> { Some(unsafe { f128_to_u32(self.inner()) }) }
-    fn to_f32(&self)    -> Option<f32> { Some(unsafe { f128_to_f32(self.inner()) }) }
-    fn to_f64(&self)    -> Option<f64> { Some(unsafe { f128_to_f64(self.inner()) }) }
-    fn to_i128(&self)   -> Option<i128> { Some(unsafe { mem::transmute::<[u8; 16], i128>(f128_to_i128(self.inner())) }) }
-    fn to_u128(&self)   -> Option<u128> { Some(unsafe { mem::transmute::<[u8; 16], u128>(f128_to_u128(self.inner())) }) }
+    fn to_i64(&self)    -> Option<i64> {
+        Some(unsafe { f128_to_i64(self.inner()) })
+    }
+    fn to_u64(&self)    -> Option<u64> {
+        Some(unsafe { f128_to_u64(self.inner()) })
+    }
+    fn to_isize(&self)  -> Option<isize> {
+        Some(unsafe { f128_to_i64(self.inner()) as isize })
+    }
+    fn to_i8(&self)     -> Option<i8> {
+        Some(unsafe { f128_to_i8(self.inner()) })
+    }
+    fn to_i16(&self)    -> Option<i16> {
+        Some(unsafe { f128_to_i16(self.inner()) })
+    }
+    fn to_i32(&self)    -> Option<i32> {
+        Some(unsafe { f128_to_i32(self.inner()) })
+    }
+    fn to_usize(&self)  -> Option<usize> {
+        Some(unsafe { f128_to_u64(self.inner()) as usize })
+    }
+    fn to_u8(&self)     -> Option<u8> {
+        Some(unsafe { f128_to_u8(self.inner()) })
+    }
+    fn to_u16(&self)    -> Option<u16> {
+        Some(unsafe { f128_to_u16(self.inner()) })
+    }
+    fn to_u32(&self)    -> Option<u32> {
+        Some(unsafe { f128_to_u32(self.inner()) })
+    }
+    fn to_f32(&self)    -> Option<f32> {
+        Some(unsafe { f128_to_f32(self.inner()) })
+    }
+    fn to_f64(&self)    -> Option<f64> {
+        Some(unsafe { f128_to_f64(self.inner()) })
+    }
+    fn to_i128(&self)   -> Option<i128> {
+        Some(unsafe {
+            mem::transmute::<[u8; 16], i128>(f128_to_i128(self.inner()))
+        })
+    }
+    fn to_u128(&self)   -> Option<u128> {
+        Some(unsafe {
+            mem::transmute::<[u8; 16], u128>(f128_to_u128(self.inner()))
+        })
+    }
 }
 
 impl FromPrimitive for f128 {
     fn from_i64(n: i64) -> Option<Self> {
-        f128_from_x!(i64, 32, n)
+        Some(f128(unsafe {
+            i64_to_f128(n)
+        }))
     }
     fn from_u64(n: u64) -> Option<Self> {
-        f128_from_x!(u64, 32, n)
+        Some(f128(unsafe {
+            u64_to_f128(n)
+        }))
     }
     fn from_isize(n: isize) -> Option<Self> {
-        f128_from_x!(isize, 32, n)
+        Some(f128(unsafe {
+            isize_to_f128(n)
+        }))
     }
     fn from_i8(n: i8) -> Option<Self> {
-        f128_from_x!(i8, 4, n)
+        Some(f128(unsafe {
+            i8_to_f128(n)
+        }))
     }
     fn from_i16(n: i16) -> Option<Self> {
-        f128_from_x!(i16, 16, n)
+        Some(f128(unsafe {
+            i16_to_f128(n)
+        }))
     }
     fn from_i32(n: i32) -> Option<Self> {
-        f128_from_x!(i32, 16, n)
+        Some(f128(unsafe {
+            i32_to_f128(n)
+        }))
     }
     fn from_usize(n: usize) -> Option<Self> {
-        f128_from_x!(usize, 32, n)
+        Some(f128(unsafe {
+            usize_to_f128(n)
+        }))
     }
     fn from_u8(n: u8) -> Option<Self> {
-        f128_from_x!(u8, 4, n)
+        Some(f128(unsafe {
+          u8_to_f128(n)
+        }))
     }
     fn from_u16(n: u16) -> Option<Self> {
-        f128_from_x!(u16, 16, n)
+        Some(f128(unsafe {
+            u16_to_f128(n)
+        }))
     }
     fn from_u32(n: u32) -> Option<Self> {
-        f128_from_x!(u32, 16, n)
+        Some(f128(unsafe {
+            u32_to_f128(n)
+        }))
     }
     fn from_f32(n: f32) -> Option<Self> {
-        // 32 is ascii space, so this buff will be filled with spaces after the number
-        let mut buf: [u8; 64] = [32u8; 64];
-        write!(&mut buf[..], "{:E}", n).expect("Failed to convert integer to string.");
-        f128::parse(str::from_utf8(&buf[..]).unwrap()).ok()
+        Some(f128(unsafe {
+            f32_to_f128(n)
+        }))
     }
     fn from_f64(n: f64) -> Option<Self> {
-        // 32 is ascii space, so this buff will be filled with spaces after the number
-        let mut buf: [u8; 64] = [32u8; 64];
-        write!(&mut buf[..], "{:E}", n).expect("Failed to convert integer to string.");
-        f128::parse(str::from_utf8(&buf[..]).unwrap()).ok()
+        Some(f128(unsafe {
+            f64_to_f128(n)
+        }))
     }
     fn from_u128(n: u128) -> Option<Self> {
-        f128_from_x!(u128, 64, n)
+        Some(f128(unsafe {
+            u128_to_f128(mem::transmute(n))
+        }))
     }
     fn from_i128(n: i128) -> Option<Self> {
-        f128_from_x!(i128, 64, n)
+        Some(f128(unsafe {
+            i128_to_f128(mem::transmute(n))
+        }))
     }
 }
 
@@ -241,26 +375,9 @@ impl Num for f128 {
 
 impl NumCast for f128 {
     fn from<T: ToPrimitive>(n: T) -> Option<Self> {
-        match n.to_i128() {
-            Some(int_component_i) => {
-                if let Some(int_component) = f128::from_i128(int_component_i) {
-                    match n.to_f64() {
-                        Some(frac_f) => {
-                            let fract = frac_f.fract();
-                            if let Some(fract) = f128::from_f64(fract) {
-                                Some(int_component + fract)
-                            } else {
-                                None
-                            }
-                        },
-                        None => None
-                    }
-                } else {
-                    None
-                }
-            },
-            None => None
-        }
+        let int_comp = f128::from_i128(n.to_i128().expect("This should never happen.")).expect("This shouldnt happen either.");
+        let frac_comp = f128::from_f64(n.to_f64().expect("This also shouldnt happen either.")).expect("This shouldnt happen.");
+        Some((frac_comp - int_comp) + int_comp)
     }
 }
 
@@ -269,42 +386,13 @@ impl Float for f128 {
 
     fn infinity() -> Self { f128::INFINITY }
 
-    #[inline(always)]
-    #[cfg(target_endian = "little")]
-    fn neg_infinity() -> Self { f128([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0xff]) }
-    #[inline(always)]
-    #[cfg(target_endian = "big")]
-    fn neg_infinity() -> Self { f128([0xff, 0xff, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) }
+    fn neg_infinity() -> Self { f128::NEG_INFINITY }
 
-    #[inline(always)]
-    #[cfg(target_endian = "little")]
-    fn neg_zero() -> Self { f128([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x80]) }
-    #[inline(always)]
-    #[cfg(target_endian = "big")]
-    fn neg_zero() -> Self { f128([0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) }
+    fn neg_zero() -> Self { f128::NEG_ZERO }
 
+    fn min_value() -> f128 { f128::MIN }
 
-    #[cfg(target_endian = "little")]
-    #[inline(always)]
-    fn min_value() -> f128 {
-        f128([0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfe, 0xff])
-    }
-    #[cfg(target_endian = "big")]
-    #[inline(always)]
-    fn min_value() -> f128 {
-        f128([0xff, 0xfe, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff])
-    }
-
-    #[cfg(target_endian = "little")]
-    #[inline(always)]
-    fn max_value() -> f128 {
-        f128([0x7f, 0xfe, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff])
-    }
-    #[cfg(target_endian = "big")]
-    #[inline(always)]
-    fn max_value() -> f128 {
-        f128([0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfe, 0x7f])
-    }
+    fn max_value() -> f128 { f128::MAX }
 
     fn min_positive_value() -> f128 { f128::MIN_POSITIVE }
 
