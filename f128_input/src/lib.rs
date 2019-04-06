f5cf0c34ded09814838e613e637c9f1a3f53cb55
Joshua Karns <jkarns275@gmail.com>
2019-04-06 18:59:14 -0400

    Now works for lists

--- f128_input/src/lib.rs
+++ f128_input/src/lib.rs
@@ -2,26 +2,38 @@ extern crate f128_internal;
 extern crate proc_macro;
 use f128_internal::f128;
 use proc_macro::TokenStream;
-use std::mem::transmute;
+use std::str;
 
-#[proc_macro]
-pub fn f128(item: TokenStream) -> TokenStream {
-    let s = item.to_string();
-    match f128::parse(&s[..]) {
-        Ok(it) => format!(
-            "::std::mem::transmute::<[u8; 16], f128>({:?})",
-            it.into_inner()
-        )
-        .parse()
-        .unwrap(),
+fn parse(s: &str) -> String {
+    let s = s.replace(' ', "");
+    let s = &s[..];
+    match f128::parse(s) {
+        Ok(it) =>
+            format!(
+                "unsafe {{ ::std::mem::transmute::<[u8; 16], f128>({:?}) }}",
+                it.into_inner()
+            ),
         Err(_) => panic!(format!("'{:?}' is not a valid f128", s)),
     }
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[proc_macro]
+pub fn f128(item: TokenStream) -> TokenStream {
+    let s = item.to_string();
+    let r = s.split(',')
+        .map(parse)
+        .collect::<Vec<String>>();
+    match r.len() {
+        0 => "".parse().unwrap(),
+        1 => r[0].parse().unwrap(),
+        _ => {
+            let len = r.iter().map(|s| s.len() + 2).sum();
+            let mut string = String::with_capacity(len);
+            for s in r.iter() {
+                string.push_str(&s[..]);
+                string.push_str(", ");
+            }
+            string.parse().unwrap()
+        }
     }
 }
